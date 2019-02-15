3671de5ad3b2812833c9d0292869517316e675e6
Michael Hawkins <michael.hawkins@kirona.com>
2019-02-15 16:04:51 +0000

    created DID hash

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <iostream>
-#include <stdlib.h>
-#include <secp256k1.h>
-#include <key.h>
+#include <cstring>
+// #include <secp256k1>
+#include "did.h"
 
 int main(int argc, char * argv[])
 {
-	printf("Generating a new private key\n");
+	std::string s_id = "did:ln:523226:1367:0";
+	std::string s_context = "https://w3id.org/did/v1";
 
-	CKey secret;
-	secret.MakeNewKey(true);
-	CPubKey pub = secret.GetPubKey();
-	uint256 hash = pub.GetHash();
-	std::cout << hash << std::endl;
+	char* c_id = new char[s_id.length() + 1];
+	char* c_context = new char[s_context.length() + 1];
+
+	std::strcpy(c_id, s_id.c_str());
+	std::strcpy(c_context, s_context.c_str());
+
+	// create a DID instance
+	DID mydid;
+	// populate instance attributes
+	mydid.setId(c_id);
+	mydid.setContext(c_context);
+
+	std::cout << mydid.toString() << std::endl;
 	return 0;
 }
